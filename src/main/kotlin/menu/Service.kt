312f5379cdf6fdef343ca83c3eb7050a5310459d
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-15 23:29:02 +0900

    feat: 코치 이름 유효성 검사(최소 2글자, 최대 4글자가 아닐 때)

--- src/main/kotlin/menu/Service.kt
+++ src/main/kotlin/menu/Service.kt
@@ -1,5 +1,6 @@
 package menu
 
+import menu.domain.coach.Coach
 import menu.view.InputView
 import menu.view.OutputView
 import java.lang.IllegalArgumentException
@@ -9,13 +10,15 @@ class Service(
     private val inputView: InputView,
 ) {
 
+    private var coaches = ""
     fun start() {
         outputView.printStartService()
 
         outputView.printRequestInputCoach()
         while (true) {
             try {
-                inputView.readCoaches()
+                coaches = inputView.readCoaches()
+                coaches.split(",").forEach { Coach(it) }
                 break
             } catch (e: IllegalArgumentException) {
                 outputView.printError(e.message)
---
+++ src/main/kotlin/menu/domain/coach/Coach.kt
@@ -0,0 +1,20 @@
+package menu.domain.coach
+
+class Coach(
+    private val name: String,
+) {
+
+    init {
+        require(checkNameLength()) { "$ERROR $NAME_LENGTH" }
+    }
+
+    private fun checkNameLength(): Boolean {
+        return name.length in 2..4
+    }
+
+    companion object {
+        const val ERROR = "[ERROR]"
+        const val NAME_LENGTH = "코치의 이름 최소 2글자, 최대 4글자 이어야 합니다."
+    }
+
+}
